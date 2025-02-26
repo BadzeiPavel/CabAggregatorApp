c18e0fb3173c9ed9aa441d867c0c7ad4d61680dd
BadzeiPavel <pavelbadzei@gmail.com>
2025-02-26 11:56:10 +0300

    add passenger crud

--- passenger-service/src/main/java/com/modsen/passenger_service/controllers/PassengerController.java
+++ passenger-service/src/main/java/com/modsen/passenger_service/controllers/PassengerController.java
@@ -4,22 +4,38 @@ import com.modsen.passenger_service.models.dtos.PassengerDTO;
 import com.modsen.passenger_service.services.PassengerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/passengers")
 @RequiredArgsConstructor
 public class PassengerController {
 
-    private final PassengerService passengerService;
+    private final PassengerService service;
 
     @PostMapping
-    public ResponseEntity<PassengerDTO> updatePassenger(@RequestBody PassengerDTO passengerDTO) {
-        PassengerDTO savedPassengerDTO = passengerService.savePassenger(passengerDTO);
+    public ResponseEntity<PassengerDTO> savePassenger(@RequestBody PassengerDTO passengerDTO) {
+        PassengerDTO savedPassengerDTO = service.savePassenger(passengerDTO);
         return ResponseEntity.ok(savedPassengerDTO);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<PassengerDTO> getPassenger(@PathVariable UUID id) {
+        PassengerDTO passengerDTO = service.getPassenger(id);
+        return ResponseEntity.ok(passengerDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PassengerDTO> updatePassenger(@PathVariable UUID id, @RequestBody PassengerDTO passengerDTO) {
+        PassengerDTO savedPassengerDTO = service.updatePassenger(id, passengerDTO);
+        return ResponseEntity.ok(savedPassengerDTO);
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<PassengerDTO> softDeletePassenger(@PathVariable UUID id) {
+        PassengerDTO passengerDTO = service.softDeletePassenger(id);
+        return ResponseEntity.ok(passengerDTO);
+    }
 }
--- passenger-service/src/main/java/com/modsen/passenger_service/services/PassengerService.java
+++ passenger-service/src/main/java/com/modsen/passenger_service/services/PassengerService.java
@@ -1,28 +1,51 @@
 package com.modsen.passenger_service.services;
 
+import com.modsen.passenger_service.exceptions.PassengerNotFoundException;
 import com.modsen.passenger_service.mappers.PassengerDTOMapper;
 import com.modsen.passenger_service.mappers.PassengerMapper;
 import com.modsen.passenger_service.models.dtos.PassengerDTO;
 import com.modsen.passenger_service.models.entities.Passenger;
 import com.modsen.passenger_service.repositories.PassengerRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class PassengerService {
 
+    private final PassengerRepository repository;
     private final PassengerMapper passengerMapper;
     private final PassengerDTOMapper passengerDTOMapper;
-    private final PassengerRepository passengerRepository;
 
     public PassengerDTO savePassenger(PassengerDTO passengerDTO) {
         Passenger passenger = passengerDTOMapper.toPassenger(passengerDTO);
+        return passengerMapper.toPassengerDTO(repository.save(passenger));
+    }
 
-        System.out.println(passenger);
+    public PassengerDTO getPassenger(UUID id) {
+        Passenger passenger = repository.findById(id)
+                .orElseThrow(() -> new PassengerNotFoundException("Passenger with id='%s' not found".formatted(id)));
+        return passengerMapper.toPassengerDTO(passenger);
+    }
 
-        return passengerMapper.toPassengerDTO(passengerRepository.save(passenger));
+    public PassengerDTO updatePassenger(UUID id, PassengerDTO passengerDTO) {
+        Passenger passenger = repository.findById(id)
+                .orElseThrow(() -> new PassengerNotFoundException("Passenger with id='%s' not found".formatted(id)));
+
+        Passenger updatedPassenger = passengerDTOMapper.toPassenger(passengerDTO);
+        updatedPassenger.setId(passenger.getId());
+
+        return passengerMapper.toPassengerDTO(repository.save(updatedPassenger));
+    }
+
+    public PassengerDTO softDeletePassenger(UUID id) {
+        Passenger passenger = repository.findById(id)
+                .orElseThrow(() -> new PassengerNotFoundException("Passenger with id='%s' not found".formatted(id)));
+        passenger.setDeleted(true);
+
+        return passengerMapper.toPassengerDTO(passenger);
     }
 
 }
