3cb2af972858476e48d976d0df1162639c33879e
BadzeiPavel <pavelbadzei@gmail.com>
2025-03-05 14:32:30 +0300

    modify poms
    modify docker-compose.yml

--- driver-service/src/main/java/com/modsen/driver_service/mappers/driver_mapper/DriverDTOMapper.java
+++ driver-service/src/main/java/com/modsen/driver_service/mappers/driver_mapper/DriverDTOMapper.java
@@ -3,11 +3,14 @@ package com.modsen.driver_service.mappers.driver_mapper;
 import com.modsen.driver_service.models.dtos.DriverDTO;
 import com.modsen.driver_service.models.entities.Driver;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.springframework.stereotype.Service;
 
 @Service
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface DriverDTOMapper {
+
+    @Mapping(target = "car", ignore = true)
     Driver toDriver(DriverDTO driverDTO);
 }
